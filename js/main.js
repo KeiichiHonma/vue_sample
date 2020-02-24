8ba317af06b289ff5db9b15dcff808c86bf992d6
keiichi-honma <keiichi1125@gmail.com>
2020-02-25 08:29:57 +0900

    a

--- js/main.js
+++ js/main.js
@@ -1,36 +1,77 @@
 (function() {
     'use strict';
     new Vue({
-        el:'#upload',
-        data: {
-            preview:'',
-            name:'',
-            styleA:true,
-            styleB:false,
+        el:'.form',
+        data:{
+            //ファイル
+            uploadFile: null
         },
-        methods: {
-            uploadFile: function(event){
-                this.styleA = true;
-                this.styleB = false;
-                const files = event.target.files ? event.target.files : event.dataTransfer.files;
-                const file = files[0];
-                const reader = new FileReader();
-                reader.onload = event => {
-                    this.preview = event.target.result;
-                };
-                reader.readAsDataURL(file);
-                this.name = files[0].name;
-                document.getElementById("upload_image").files = files;
+        methods:{
+            selectedFile: function(e) {
+                // 選択された File の情報を保存しておく
+                e.preventDefault();
+                let files = e.target.files;
+                this.uploadFile = files[0];
             },
-            changeStyle: function(event,flag){
-                if(flag=='ok'){
-                    this.styleA = false;
-                    this.styleB = true;
-                }else{
-                    this.styleA = true;
-                    this.styleB = false;
-                }
+            //ファイル送信処理
+            upload:function(){
+                // FormData を利用して File を POST する
+                let formData = new FormData();
+                formData.append('file', this.uploadFile);
+                console.log(this.uploadFile)
+                let config = {
+                    withCredentials: true,
+                    headers: {
+                        'content-type': 'multipart/form-data'
+                    }
+                };
+                axios
+                    .post('http://localhost:8080/upload', formData, config)
+                    .then(function(response) {
+                        // response 処理
+                        console.log(response)
+                    })
+                    .catch(function(error) {
+                        // error 処理
+                        console.log(error)
+                    })
             },
         },
     });
   })();
+// (function() {
+//     'use strict';
+//     new Vue({
+//         el:'#upload',
+//         data: {
+//             preview:'',
+//             name:'',
+//             styleA:true,
+//             styleB:false,
+//         },
+//         methods: {
+//             uploadFile: function(event){
+//                 this.styleA = true;
+//                 this.styleB = false;
+//                 const files = event.target.files ? event.target.files : event.dataTransfer.files;
+//                 const file = files[0];
+//                 const reader = new FileReader();
+//                 reader.onload = event => {
+//                     this.preview = event.target.result;
+//                 };
+//                 reader.readAsDataURL(file);
+//                 this.name = files[0].name;
+//                 document.getElementById("upload_image").files = files;
+//             },
+//             changeStyle: function(event,flag){
+//                 if(flag=='ok'){
+//                     this.styleA = false;
+//                     this.styleB = true;
+//                 }else{
+//                     this.styleA = true;
+//                     this.styleB = false;
+//                 }
+//             },
+//         },
+//     });
+//   })();
